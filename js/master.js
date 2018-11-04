12703a4e1d8527fdf8799c00d8fb395fb3c5fec4
Luis Ángel Ávila Torres <luisangelavilatorres@gmail.com>
2018-11-04 16:30:48 -0500

    Agregando descripción del Pokemon

--- js/master.js
+++ js/master.js
@@ -1,6 +1,8 @@
 const $numberForm = document.querySelector('#number-form')
 const $nameForm = document.querySelector('#name-form')
 const $image = document.querySelector('#image')
+const $text = document.querySelector('#name-form input')
+const $textarea = document.querySelector('#textarea')
 
 async function getData(entrypoint, param) {
   const response =  await fetch(`https://pokeapi.co/api/v2/${entrypoint}/${param}/`)
@@ -13,7 +15,15 @@ $numberForm.addEventListener('submit', async (ev) => {
   const form = new FormData($numberForm)
   const id = form.get('id')
   const pokemon = await getData('pokemon', id)
-  const text = document.querySelector('#name-form input')
-  text.value = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+
+  // Establecer nombre del Pokemon
+  $text.value = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+
+  // Establecer imagen de Pokemon
   $image.setAttribute('src', pokemon.sprites.front_default)
+
+  // Establecer descripción del Pokemon
+  const species = await getData('pokemon-species',id)
+  const flavor = species.flavor_text_entries.find((entry) => entry.language.name === 'es').flavor_text
+  $textarea.value = flavor
 })
