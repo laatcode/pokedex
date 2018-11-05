68a46b5648407da054c50c30c7db88f67ffafeab
Luis Ángel Ávila Torres <luisangelavilatorres@gmail.com>
2018-11-04 20:58:04 -0500

    Agregando consulta de Pokemon por nombre

--- js/master.js
+++ js/master.js
@@ -1,6 +1,7 @@
 const $numberForm = document.querySelector('#number-form')
 const $nameForm = document.querySelector('#name-form')
 const $image = document.querySelector('#image')
+const $id = document.querySelector('#number-form input')
 const $text = document.querySelector('#name-form input')
 const $textarea = document.querySelector('#textarea')
 const speach = window.speechSynthesis
@@ -12,11 +13,11 @@ async function getData(entrypoint, param) {
   return pokemon
 }
 
-$numberForm.addEventListener('submit', async (ev) => {
-  ev.preventDefault()
-  const form = new FormData($numberForm)
-  const id = form.get('id')
-  const pokemon = await getData('pokemon', id)
+async function formAction(param){
+  const pokemon = await getData('pokemon', param)
+
+  // Establecer id del Pokemon
+  $id.value = pokemon.id
 
   // Establecer nombre del Pokemon
   $text.value = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
@@ -25,7 +26,7 @@ $numberForm.addEventListener('submit', async (ev) => {
   $image.setAttribute('src', pokemon.sprites.front_default)
 
   // Establecer descripción del Pokemon
-  const species = await getData('pokemon-species',id)
+  const species = await getData('pokemon-species',pokemon.id)
   const flavor = species.flavor_text_entries.find((entry) => entry.language.name === 'es').flavor_text
   $textarea.value = flavor
 
@@ -35,4 +36,18 @@ $numberForm.addEventListener('submit', async (ev) => {
   utterance.lang = 'es'
 
   speach.speak(utterance)
+}
+
+$numberForm.addEventListener('submit', async (ev) => {
+  ev.preventDefault()
+  const form = new FormData($numberForm)
+  const id = form.get('id').toLowerCase()
+  formAction(id)
+})
+
+$nameForm.addEventListener('submit', (ev) => {
+  ev.preventDefault()
+  const form = new FormData($nameForm)
+  const name = form.get('name').toLowerCase()
+  formAction(name)
 })
